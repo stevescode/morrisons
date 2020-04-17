4d163e4d184c92c152394fd3ccf40a4c5337b53d
stevescode <steves106@hotmail.com>
2020-04-17 22:12:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -11,36 +11,58 @@
 (function() {
     'use strict';
 
-    const freq = 15000; // frequency of refresh
-    var listy = document.getElementsByTagName("span");
-    var curr = Date.now();
-    var notFound = 0;
-    var found = 0;
-
-    console.log(listy);
+    // let's get a refresh going to keep checking for changes and running this script
+    const freq = 15000; // 15 seconds
     setTimeout(function(){ location.reload(); }, freq);
 
-    for (var i = 11; i < 32; i++) {
-        console.log(listy[i].innerText);
+    // reduce size of document being worked with
+    var content = document.getElementById('content');
 
-        if (listy[i].innerText.includes("Unava")) {
-            notFound = notFound + 1;
-            listy[i].style.backgroundColor = "red";
+    // this gives every unordered list containing list items in the content div
+    var listItems = content.querySelectorAll('ul>li');
+
+    console.log(listItems);
+
+    // 22 slots on screen - let's record what's happening first
+    let availableSlots = 0;
+    let unavailableSlots = 0;
+    var curr = Date.now();
+
+    for (let i = listItems.length -1; i > 0; i--) {
+        
+        if (listItems[i].innerHTML.includes("Unavail")) {
+            unavailableSlots = unavailableSlots + 1;
         }
-        else {
-            found = found + 1;
-            listy[i].style.backgroundColor = "green";
-            listy[i].style.fontSize = "xx-large";
+
+        else if (listItems[i].innerHTML.includes("selectDeliveryTime")) {
+            availableSlots = availableSlots + 1;
         }
 
     }
 
-    if ( notFound > 0 ) {
-        GM_setValue(curr, "Unavailable Slots: " + notFound);
+    if ( availableSlots >= 0 ) {
+        GM_setValue(curr, "Available Slots: " + availableSlots);
     }
 
-    if ( found > 0) {
-        GM_setValue(curr, "Available Slots: " + found);
+    // let's iterate through the Nodelist in reverse length minus 1 because index starts at zero
+    // currently minus 14 because we only want to book something within the next 9 days
+    // we're driving in reverse because we want the click logic to start from the furthest slot out
+    for (let i = listItems.length -10; i > 0; i--) {
+
+        // this pulls out the delivery slots that are unavailable and highlights them red on screen
+        if (listItems[i].innerHTML.includes("Unavail")) {
+            console.log(listItems[i]);
+            listItems[i].style.backgroundColor = "red";
+        }
+
+        // this pulls out the delivery slots that are available and clicks to the next page
+        else if (listItems[i].innerHTML.includes("selectDeliveryTime")) {
+            console.log(listItems[i]);
+            listItems[i].style.backgroundColor = "green";
+            
+            // this'll click the first avaiable slot it finds
+            listItems[i].querySelector("a").click();
+        }
     }
 
 })();
